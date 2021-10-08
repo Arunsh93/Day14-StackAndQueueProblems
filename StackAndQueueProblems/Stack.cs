1bde5444a5685b19cfc6f28f264cf84d0c91b12e
Arunsh93 <Arunhosur93@gmail.com>
2021-10-08 16:44:57 +0530

    Pushing into Stack

---
+++ StackAndQueueProblems/Stack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueueProblems
+{
+    class Stack
+    {
+        internal Node top;
+
+        public Stack()
+        {
+            this.top = null;
+        }
+
+        //Pushing Elemnts in the Stack
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            if(top == null)
+            {
+                top = newNode;
+            }
+            else
+            {
+                newNode.Next = top;
+                top = newNode;
+            }
+            Console.WriteLine("{0} Pushed to Stack ", data);
+        }
+    }
+}
