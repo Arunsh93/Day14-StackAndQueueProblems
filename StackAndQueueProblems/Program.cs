f58baedc944b64dc6b8f3ce0b6e5eaedcc99826e
Arunsh93 <Arunhosur93@gmail.com>
2021-10-08 17:12:25 +0530

    Peak and Pop the Stack Element

--- StackAndQueueProblems/Program.cs
+++ StackAndQueueProblems/Program.cs
@@ -10,6 +10,13 @@ namespace StackAndQueueProblems
             stack.Push(56);
             stack.Push(30);
             stack.Push(70);
+
+            int peakElement = stack.Peak();
+            Console.WriteLine("The Peak(top) Element of the Stack is: " + peakElement);
+
+            stack.Pop();
+            Console.WriteLine("After Poping the Element: ");
+            stack.Display();
         }
     }
 }
--- StackAndQueueProblems/Stack.cs
+++ StackAndQueueProblems/Stack.cs
@@ -28,5 +28,55 @@ namespace StackAndQueueProblems
             }
             Console.WriteLine("{0} Pushed to Stack ", data);
         }
+
+        //checking stack is empty or not
+        public bool isEmpty()
+        {
+            return top == null;
+        }
+
+        //peek element(top element in the Stack)
+        public int Peak()
+        {
+            if (!isEmpty())
+            {
+                return top.data;
+            }
+            else
+            {
+                Console.WriteLine("Stack is Empty");
+                return -1;
+            }
+        }
+
+        public void Pop()
+        {
+            if(top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+                return;
+            }
+            top = top.Next;
+        }
+
+        public void Display()
+        {
+            if(top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+                return;
+            }
+            else
+            {
+                Node temp = top;
+                while(temp != null)
+                {
+                    Console.WriteLine("{0}->", temp.data);
+                    temp = temp.Next;
+                }
+
+                Console.WriteLine();
+            }
+        }
     }
 }
